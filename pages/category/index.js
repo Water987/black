aef7b185f49e8c0ce81140f22ecd86e4fc6f1a4b
Water987 <jailoriented@qq.com>
2020-12-09 22:12:28 +0800

    完成商城主页 接入分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,20 +1,35 @@
 // pages/category/index.js
+import { request } from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList:[],
+    rightContent:[],
   },
+  Cates:[],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCats()
   },
+  /* 获取页面分类数据 */
+  getCats() {
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"})
+    .then(result=>{
+      this.Cates = result.data.message;
 
+      // 构造左侧菜单栏数据
+      let leftMenuList = this.Cates.map(v=>v.cat_name);
+      this.setData({leftMenuList});
+
+      // 构造右侧内容栏数据
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,7 @@
 //index.js
 //获取应用实例
+// 0 引入用来 发送请求的 方法
+import { request } from "../../request/index.js";
 
 Page({
 
@@ -7,62 +9,60 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    swiperList:[],
+    cateList:[],
+    floorList:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    //1 发送一部请求获取轮播图数据
+    /* var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        this.setData({
+          swiperList: result.data.message
+        })
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    }); */
+    // 2 使用 es6 的Promise 优化异步请求
+    this.getSwiperList();
+    // 获取首页分类导航数据
+    this.getCateList();
+    this.getFloorList();
+  },
+  // 获取首页轮播图数据
+  getSwiperList() {
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result=>{
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
+  },
+  // 获取首页分类导航数据
+  getCateList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    .then(result=>{
+
+      this.setData({
+        cateList: result.data.message
+      })
+    })
+  },
+  // 获取首页楼层数据
+  getFloorList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    .then(result=>{
+
+      this.setData({
+        floorList: result.data.message
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
